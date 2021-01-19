988483fa6a2c34cd0852bb016c095d71e412465f
Sean Sundberg <seansund@us.ibm.com>
2021-01-18 23:59:11 -0600

    Provisions cos bucket

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "ibm" {
+  version = ">= 1.17.0"
+}
+
+data "ibm_resource_group" "resource_group" {
+  name = var.resource_group_name
+}
+
+data "ibm_resource_instance" "cos_instance" {
+  name = var.instance-name
+  location = var.instance-region
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_cos_bucket" "bucket" {
+  bucket_name          = var.name
+  resource_instance_id = data.ibm_resource_instance.cos_instance
+  region_location      = var.region
+  storage_class        = var.storage-class
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,30 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group where the cluster has been provisioned."
+}
 
+variable "instance-region" {
+  type        = string
+  description = "The region where the object storage instance has been provisioned"
+}
+
+variable "instance-name" {
+  type        = string
+  description = "The name of the object storage instance"
+}
+
+variable "region" {
+  type        = string
+  description = "The region where the bucket where be created"
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the bucket that will be created"
+}
+
+variable "storage-class" {
+  type        = string
+  description = "The storage class for the bucket"
+  default     = "flex"
+}
