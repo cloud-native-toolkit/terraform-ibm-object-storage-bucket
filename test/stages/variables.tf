cdbfde5dfd0086cab71f2983065be7fd3d8991ec
Sean Sundberg <seansund@us.ibm.com>
2021-05-04 21:41:30 -0500

    Adds support for encrypting the COS bucket (#4)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -2,7 +2,7 @@
 # Resource Group Variables
 variable "resource_group_name" {
   type        = string
-  description = "Existing resource group where the COS instance will be provisioned."
+  description = "Existing resource group where the IKS cluster will be provisioned."
 }
 
 variable "ibmcloud_api_key" {
@@ -19,4 +19,28 @@ variable "name_prefix" {
   type        = string
   description = "Prefix name that should be used for the cluster and services. If not provided then resource_group_name will be used"
   default     = ""
+}
+
+variable "hpcs_name" {
+  type        = string
+}
+
+variable "kms_id" {
+  type        = string
+}
+
+variable "kms_key_name" {
+  type        = string
+}
+
+variable "kms_enabled" {
+  type        = bool
+}
+
+variable "hpcs_region" {
+  type        = string
+}
+
+variable "hpcs_resource_group_name" {
+  type        = string
 }
