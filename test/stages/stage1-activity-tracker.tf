26727f3d942a8030e396d43aff77f8c5487161b2
Sean Sundberg <seansund@us.ibm.com>
2021-05-11 16:45:00 -0500

    Connects bucket with Activity Tracker and Monitoring services (#18)
    - Adds activity_tracker_crn and metrics_monitoring_crn input variables
    - Connects the bucket to the activity tracker and monitoring instances if they are provided
    
    Fixes #15
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ test/stages/stage1-activity-tracker.tf
@@ -0,0 +1,7 @@
+module "activity_tracker" {
+  source = "github.com/cloud-native-toolkit/terraform-ibm-activity-tracker"
+
+  resource_group_name      = module.resource_group.name
+  resource_location        = var.region
+  provision                = true
+}
--- test/stages/stage3-cos-bucket.tf
+++ test/stages/stage3-cos-bucket.tf
@@ -13,11 +13,13 @@ resource "random_string" "random" {
 module "dev_cos_bucket" {
   source = "./module"
 
-  resource_group_name = module.resource_group.name
-  cos_instance_id     = module.cos.id
-  name_prefix         = var.name_prefix
-  ibmcloud_api_key    = var.ibmcloud_api_key
-  name                = "my-test-bucket-${random_string.random.result}"
-  region              = var.region
-  kms_key_crn         = module.hpcs_key.crn
+  resource_group_name    = module.resource_group.name
+  cos_instance_id        = module.cos.id
+  name_prefix            = var.name_prefix
+  ibmcloud_api_key       = var.ibmcloud_api_key
+  label                  = "bucket-${random_string.random.result}"
+  region                 = var.region
+  kms_key_crn            = module.hpcs_key.crn
+  activity_tracker_crn   = module.activity_tracker.crn
+  metrics_monitoring_crn = module.monitoring.crn
 }
