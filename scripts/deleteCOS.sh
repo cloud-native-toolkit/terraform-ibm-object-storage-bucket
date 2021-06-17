dac7400c9bc4e25447e7a252b974ab0029a73807
Sean Sundberg <seansund@us.ibm.com>
2021-06-17 16:37:13 -0500

    Updates deleteCOS.sh script to be used as a general utility (#23)
    - Adds region as a required argument (not just an environment variable)
    - Uses the version of minio from the path if available
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/deleteCOS.sh
+++ scripts/deleteCOS.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-BUCKET_NAME="$1"
-KEY_ID="$2"
+REGION="$1"
+BUCKET_NAME="$2"
+KEY_ID="$3"
 
-if [[ -z "$BUCKET_NAME" ]] || [[ -z "$KEY_ID" ]]; then
-  echo "Usage: deleteCOS.sh BUCKET_NAME KEY_ID"
+if [[ -z "${REGION}" ]] || [[ -z "$BUCKET_NAME" ]] || [[ -z "$KEY_ID" ]]; then
+  echo "Usage: deleteCOS.sh REGION BUCKET_NAME KEY_ID"
   exit 0
 fi
 
 if ! ibmcloud account show 1> /dev/null 2> /dev/null; then
+  echo "Not logged in"
+
+  if [[ -z "${IBMCLOUD_API_KEY}" ]] || [[ -z "${RESOURCE_GROUP}" ]]; then
+    echo "In order to log in, IBMCLOUD_API_KEY and RESOURCE_GROUP must be provided as environment variables"
+    exit 1
+  fi
+
+  echo "Logging in to IBM Cloud account"
   ibmcloud login --apikey "${IBMCLOUD_API_KEY}" -g "${RESOURCE_GROUP}" -r "${REGION}"
 else
+  echo "Current IBM Cloud target"
   ibmcloud target
 fi
 
 JQ=$(command -v jq || command -v ./bin/jq)
 
 if [[ -z "${JQ}" ]]; then
-  echo "jq missing. Installing"
+  echo "jq missing. Installing for linux-amd64 architecture..."
   mkdir -p ./bin && curl -Lo ./bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
   JQ="${PWD}/bin/jq"
 fi
@@ -34,22 +44,29 @@ if [[ -z "$ENDPOINT_URL" ]] || [[ -z "$ACCESS_KEY" ]] || [[ -z "$SECRET_KEY" ]];
 fi
 
 echo "Getting endpoints from $ENDPOINT_URL"
-ENDPOINT_HOST=$(curl -sL "$ENDPOINT_URL" | "$JQ" --arg REGION "$REGION" -r '."service-endpoints".regional[$REGION]["public"][$REGION]')
-ENDPOINT="https://${ENDPOINT_HOST}"
-
-VAR1=$(command -v ./mc)
-if [[ -z "$VAR1" ]]; then
-  echo "Installing MinIO"
-  wget https://dl.min.io/client/mc/release/linux-amd64/mc
-  echo "MinIO has been installed"
+ENDPOINT_HOST=$(curl -sL "$ENDPOINT_URL" | "$JQ" --arg REGION "$REGION" -r '."service-endpoints".regional[$REGION]["public"][$REGION] // empty')
+if [[ -z "${ENDPOINT_HOST}" ]]; then
+  echo "Unable to find ENDPOINT_HOST"
+  exit 1
 fi
 
-chmod +x mc
-echo "mc has permissions"
+ENDPOINT="https://${ENDPOINT_HOST}"
 
-./mc config host add IBMCOS "${ENDPOINT}" "$ACCESS_KEY" "$SECRET_KEY" || exit 0
-echo "Mc configration done"
+echo "COS endpoint: ${ENDPOINT}"
+
+MC=$(command -v mc || command -v ./bin/mc)
+if [[ -z "${MC}" ]]; then
+  echo "MinIO missing. Installing for linux-amd64 architecture..."
+  mkdir -p ./bin && curl -Lo ./bin/mc https://dl.min.io/client/mc/release/linux-amd64/mc
+  echo "MinIO has been installed"
+
+  chmod +x ./bin/mc
+  MC="$(cd ./bin || exit 1; pwd -P)/mc"
+fi
+
+${MC} config host add IBMCOS "${ENDPOINT}" "$ACCESS_KEY" "$SECRET_KEY" || exit 0
+echo "MinIO configuration done for COS"
 
 # delete contents
-./mc rm "IBMCOS/${BUCKET_NAME}/" --recursive --force || exit 0
-echo "deleted all bucket contents" 
+${MC} rm "IBMCOS/${BUCKET_NAME}/" --recursive --force || exit 0
+echo "Deleted all bucket contents"
