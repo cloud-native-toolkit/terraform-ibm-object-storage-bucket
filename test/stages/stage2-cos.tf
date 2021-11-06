b0a91074c6345744eb763c0d9ee38a56ca2a0b97
Sean Sundberg <seansund@us.ibm.com>
2021-11-06 08:45:03 -0500

    Updates handling of provider config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-cos.tf
+++ test/stages/stage2-cos.tf
@@ -1,7 +1,6 @@
 module "cos" {
   source = "github.com/ibm-garage-cloud/terraform-ibm-object-storage.git"
 
-  ibmcloud_api_key    = var.ibmcloud_api_key
   resource_group_name = var.resource_group_name
   name_prefix         = var.name_prefix
   name                = "test-cos-instance"
