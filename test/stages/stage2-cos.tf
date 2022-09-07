05cc7ecb05ac32f088c5e2a01c535de3f4eaf81b
Bala Sivasubramanian <bala@us.ibm.com>
2022-09-07 09:18:42 -0500

    updates to README (#33)
    * updates to README
    
    * fix the compile errors

--- test/stages/stage2-cos.tf
+++ test/stages/stage2-cos.tf
@@ -1,7 +1,7 @@
 module "cos" {
   source = "github.com/ibm-garage-cloud/terraform-ibm-object-storage.git"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.name
   name_prefix         = var.name_prefix
   name                = "test-cos-instance"
   provision = true
